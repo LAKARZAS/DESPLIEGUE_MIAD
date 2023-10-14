5a2e93966b0261ded18eeb3583f8983f732da923
LAKARZAS <129604724+LAKARZAS@users.noreply.github.com>
2023-10-13 20:26:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -245,5 +245,5 @@ def update_output_div(date, hour, proy):
 
 
 # Run the server
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if _name_ == "_main_":
+    app.run_server(host='0.0.0.0',debug=True)
